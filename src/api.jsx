c0893bc88c145249667969bead215f1dfe075610
nehacc <nehachoudhary1104@gmail.com>
2024-07-31 10:46:28 +0530

    set env

--- src/api.jsx
+++ src/api.jsx
@@ -2,11 +2,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL, 
+  baseURL: '${import.meta.env.REACT_APP_API_BASE_URL}/', 
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
 });
+
 
 export default api;
