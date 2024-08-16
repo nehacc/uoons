942ced7899c883bc306f80f3d7637871cfff7e8c
Akshit <akshitchauhan2953@gmail.com>
2024-08-16 18:58:27 +0530

    16-8-24

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -171,7 +171,6 @@ const ProductsContainer = (props) => {
                       e.stopPropagation();
                       navigate(`/Checkout/${item.pid}`) }}
 
-                    href="#_"
                     className="relative rounded p-2 py-1 overflow-hidden group bg-orange-500 hover:bg-gradient-to-r hover:from-orange-500 hover:to-orange-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-orange-400 transition-all ease-out duration-300"
                   >
                     <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
